f90f8017cc8cc22c6d0a99a09cd7a5cc7d619f0b
Dominic Palmer <dominic.palmer@outlook.com>
2022-05-14 22:53:17 +0100

    Moved vimscript into common

--- windows/install/wsl.sh
+++ windows/install/wsl.sh
@@ -31,8 +31,8 @@ nvm install node
 # Create symbolic links in WSl that link up to dotfiles in Windows at /mnt/c
 DOTFILES=/mnt/c/Users/Dom/root/cs/projects/dotfiles
 
-ln -sf $DOTFILES/unix/nvim $HOME/.config/nvim
-ln -sf $DOTFILES/unix/nvim_vimscript $HOME/.config/nvim_vimscript
+ln -sf $DOTFILES/common/nvim $HOME/.config/nvim
+ln -sf $DOTFILES/common/nvim_vimscript $HOME/.config/nvim_vimscript
 ln -sf $DOTFILES/unix/zsh/.zshrc $HOME/.zshrc
 ln -sf $DOTFILES/unix/tmux/.tmux.conf $HOME/.tmux.conf
 ln -sf $DOTFILES/unix/tmux/tmux_change_theme.sh $HOME/tmux_change_theme.sh
